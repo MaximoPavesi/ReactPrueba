a5cc31e2082ec4eb7006e7b3139360f27665406d
Maximo Pavesi <machipavesi@gmail.com>
2023-03-06 19:35:02 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { Header } from './componentes/Header/index';
+import { ProductosLista } from './componentes/Productos/index'; 
+import 'boxicons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <ProductosLista />
     </div>
   );
 }
