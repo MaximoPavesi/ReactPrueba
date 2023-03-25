74946b435307204ca79c93cf01476afb706d01bc
Maximo Pavesi <machipavesi@gmail.com>
2023-03-25 16:27:34 -0300

    arreglos de componentes

---
+++ src/utils/images.js
@@ -0,0 +1,29 @@
+import Nike01 from "../assets/img01.jpg";
+import DunkOf from "../assets/img02.jpg";
+import AF1Supreme from "../assets/img03.jpg";
+import Nike02 from "../assets/img04.jpg";
+import DunkViolet from "../assets/img05.jpg";
+import DunkOfRed from "../assets/img06.jpg";
+import Nike03 from "../assets/img07.jpg";
+import AF1Blue from "../assets/img08.jpg";
+import AirMax from "../assets/img09.jpg";
+import DunkHigh from "../assets/img010.jpg";
+import Nike04 from "../assets/img011.jpg"
+import Logo from "../assets/inicio.jpg";
+import Banner01 from "../assets/Nike.jpg";
+
+export const Images = {
+  Nike01,
+  DunkOf,
+  AF1Supreme,
+  Nike02,
+  DunkViolet,
+  DunkOfRed,
+  Nike03,
+  AF1Blue,
+  AirMax,
+  DunkHigh,
+  Nike04,
+  Logo,
+  Banner01,
+};
