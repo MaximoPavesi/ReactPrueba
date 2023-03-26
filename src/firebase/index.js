de6fb28ae24cfe7c5868c8a411f9d15841e4d72e
Maximo Pavesi <machipavesi@gmail.com>
2023-03-26 20:33:42 -0300

    arreglos

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,14 +1,15 @@
-import * as firebase from "firebase/app";
-import "firebase/firestore";
+import { initializeApp } from "firebase/app";
 
-import { firebaseConfig } from "../config/firebase.config";
+const firebaseConfig = {
+  apiKey: "AIzaSyAp-YKx4bJw8df3GQe9NE3ESUAAvt2-qco",
+  authDomain: "e-commerce-coder-6194e.firebaseapp.com",
+  projectId: "e-commerce-coder-6194e",
+  storageBucket: "e-commerce-coder-6194e.appspot.com",
+  messagingSenderId: "848906970337",
+  appId: "1:848906970337:web:74bd6ceb4bd4e0895cc3a0"
+};
 
-const app = firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-export function getFirebase() {
-  return app;
-}
 
-export function getFirestore() {
-  return firebase.firestore(app);
-}
+import {doc, getDoc, getFirestore} from "firebase/firestore";
